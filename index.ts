656d19c9682d838070714231ae3333420cbd4bb6
arlekin6767-sketch <arlekin6767@gmail.com>
2026-07-24 13:27:50 +0500

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { startTelegramBot } from "./telegram-bot";
-
-console.log("Starting NFT Gift Price Bot...");
-
-startTelegramBot().catch((error) => {
-  console.error("Fatal error starting bot:", error);
-  process.exit(1);
-});
